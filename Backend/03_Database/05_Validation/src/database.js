bf30f4a053cb63aab818a2fb273d92d07b6ce499
Ayush Raj <148717357+ayushh002@users.noreply.github.com>
2025-09-15 12:22:00 +0530

    Update database.js

--- Backend/03_Database/05_Validation/src/database.js
+++ Backend/03_Database/05_Validation/src/database.js
@@ -1,11 +1,12 @@
 const mongoose = require('mongoose');
 
-const url = 'mongodb+srv://ayushh002:Anshuman%401104@codingclub.4tygwex.mongodb.net/backend';
+const url = 'mongodb+srv://ayushh002:userPassword@codingclub.4tygwex.mongodb.net/backend';
 
 async function main(){
 
     await mongoose.connect(url);
 
 }
+
 
 module.exports = main;
